ee6c14526ba36388b5d95ff8a10dbdf9f4364291
Damjan Krivacevic <damjan.krivacevic@gmail.com>
2022-07-19 00:05:38 +0200

    Refactor tests

--- tests/Feature/ViewResourceListTest.php
+++ tests/Feature/ViewResourceListTest.php
@@ -13,48 +13,40 @@ class ViewResourceListTest extends TestCase
 {
     use DatabaseMigrations;
 
+    private $pdfResource;
+
     public function setUp() :void
     {
         parent::setUp();
 
         $this->seed();
-    }
 
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_the_application_returns_a_successful_response()
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $this->pdfResource = $this->makePdfResource();
     }
 
     /** @test */
     public function visitor_can_view_a_resource_listing()
     {
-
-        $resourcePdf = ResourceItem::factory()
-            ->has(ResourceAttachment::factory([
-                'filename' => 'Some name',
-                'path' => 'some/cool/path/here.txt'
-            ]))
-            ->create([
-                'title' => 'Some resource title',
-                'resource_item_type_id' => ResourceItemType::firstWhere('type', 'PDF')->id
-            ]);
-
         $this->assertDatabaseHas('resource_items', [
-            'title' => 'Some resource title',
+            'title' => 'Some pdf resource title',
         ]);
 
         $view = $this->get('/');
 
         $view->assertStatus(200);
-        $view->assertSee('Some resource title');
+        $view->assertSee('Some pdf resource title');
+    }
 
-
+    private function makePdfResource()
+    {
+        return ResourceItem::factory()
+            ->has(ResourceAttachment::factory([
+                'filename' => 'Some name',
+                'path' => 'some/cool/path/here.txt'
+            ]))
+            ->create([
+                'title' => 'Some pdf resource title',
+                'resource_item_type_id' => ResourceItemType::firstWhere('type', 'PDF')->id
+            ]);
     }
 }
