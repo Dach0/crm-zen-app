8853374085bff69bc3950f5291ffea2baf494fc6
Damjan Krivacevic <damjan.krivacevic@gmail.com>
2022-07-21 22:10:10 +0200

    Disable resource type update and show proper pdf file name on update

--- resources/js/components/recource-items/CreateEditModal.vue
+++ resources/js/components/recource-items/CreateEditModal.vue
@@ -30,12 +30,16 @@
                                                                 id="resource-type"
                                                                 v-model="resourceItem.resource_item_type_id"
                                                                 name="resource-type"
+                                                                :disabled="editingItem"
                                                                 autocomplete="resource-type"
                                                                 class="max-w-lg block focus:ring-indigo-500 focus:border-indigo-500 w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                                                             >
                                                                 <option value="">Choose type</option>
                                                                 <option v-for="item in resourceItemTypes" :value="item.id">{{ item.type }}</option>
                                                             </select>
+                                                            <div v-if="editingItem" class="text-sm text-gray-500 mt-1">
+                                                                You are not able to update resource type
+                                                            </div>
                                                             <div
                                                                 class="text-sm text-red-500 mt-1"
                                                                 v-for="message in validationErrors?.resource_item_type_id"
@@ -64,7 +68,7 @@
                                                         <div class="mt-1 sm:mt-0 sm:col-span-2">
                                                             <div class="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
                                                                 <div class="space-y-1 text-center">
-                                                                    <p v-if="resourceItem.pdf_file">{{ resourceItem.pdf_file.name }}</p>
+                                                                    <p v-if="resourceItem.pdf_file" class="mb-2">{{ resourceItem.pdf_file.name  || resourceItem.pdf_file }}</p>
                                                                     <svg v-else class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
                                                                         <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                                                                     </svg>
@@ -302,7 +306,7 @@ const updateResourceItem = async (resourceItem) => {
 
     axios.post('api/resource-items/' + props.editingItem.id , serializedResourceItem)
         .then(response => {
-            console.log(response)
+            emit('resourceCreated')
         })
         .catch(error => {
             if (error.response?.data) {
